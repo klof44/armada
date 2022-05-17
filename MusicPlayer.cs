4c42586d786bb4ecb44de240fff3b8de4bfe7dda
klof44 <klof4four@gmail.com>
2022-05-16 21:21:34 -0500

    voice channel slash commands fix

--- MusicPlayer.cs
+++ MusicPlayer.cs
@@ -6,12 +6,18 @@ using DSharpPlus.CommandsNext;
 using DSharpPlus.Lavalink;
 using System.Linq;
 using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
 
 namespace armada
 {
 	public class MusicPlayer
 	{
 		internal MusicPlayer(ulong guild, CommandContext ctx)
+		{
+			node = ctx.Client.GetLavalink().ConnectedNodes.FirstOrDefault().Value;
+			token = new CancellationTokenSource();
+		}
+		internal MusicPlayer(ulong guild, InteractionContext ctx)
 		{
 			node = ctx.Client.GetLavalink().ConnectedNodes.FirstOrDefault().Value;
 			token = new CancellationTokenSource();
@@ -151,6 +157,144 @@ namespace armada
 					embed.AddField($"{track.Title} - {track.Author}", track.Length.ToString());
 				}
 				await ctx.RespondAsync(embed);
+			}
+		}
+
+		// InteractionContext versions
+
+			public static async Task Play(LavalinkTrack track, InteractionContext ctx)
+			{
+			if (musicPlayers.ContainsKey(ctx.Guild.Id))
+				{
+				try
+				{
+					musicPlayers[ctx.Guild.Id].musicQueue.Enqueue(track);
+				}
+				catch { }
+				
+				DiscordEmbedBuilder embed = new()
+				{
+					Color = DiscordColor.HotPink,
+					Title = "Added to Queue",
+				};
+				embed.AddField($"{track.Title} - {track.Author}", track.Length.ToString());
+				await ctx.CreateResponseAsync(embed);
+
+				if (!musicPlayers[ctx.Guild.Id].musicPlaying)
+				{
+					await musicPlayers[ctx.Guild.Id].Start(track, ctx);
+				}
+			}
+			else
+			{
+				musicPlayers.Add(ctx.Guild.Id, new MusicPlayer(ctx.Guild.Id, ctx));
+
+				// I can't figure out why but putting a try catch here fixes the System.Private.CoreLib.dll issue
+				try
+				{
+					musicPlayers[ctx.Guild.Id].musicQueue.Enqueue(track);
+				}
+				catch { }
+
+				DiscordEmbedBuilder embed = new()
+				{
+					Color = DiscordColor.HotPink,
+					Title = "Added to Queue",
+				};
+				embed.AddField($"{track.Title} - {track.Author}", track.Length.ToString());
+				await ctx.CreateResponseAsync(embed);
+
+
+				await musicPlayers[ctx.Guild.Id].Start(track, ctx);
+			}
+		}
+
+		private async Task Start(LavalinkTrack track, InteractionContext ctx)
+		{
+			if (!musicPlaying)
+			{
+				GuildConnection = node.ConnectAsync(ctx.Member.VoiceState.Channel).Result;
+			}
+			
+			while (musicQueue.Count > 0)
+			{
+				var CurrentTrack = musicQueue.Dequeue();
+				await GuildConnection.SetVolumeAsync(50);
+				await GuildConnection.PlayAsync(CurrentTrack);
+				musicPlaying = true;
+
+				DiscordEmbedBuilder embed = new()
+				{
+					Color = DiscordColor.HotPink,
+					Title = "Now Playing",
+				};
+				embed.AddField($"{CurrentTrack.Title} - {CurrentTrack.Author}", CurrentTrack.Length.ToString());
+				await ctx.Channel.SendMessageAsync(embed);
+
+				await Task.Delay(CurrentTrack.Length + TimeSpan.FromSeconds(1), token.Token);
+			}
+			musicPlaying = false;
+
+			await Stop(ctx);
+		}
+
+		public static async Task Stop(InteractionContext ctx)
+		{
+			if (musicPlayers.ContainsKey(ctx.Guild.Id))
+			{
+				musicPlayers[ctx.Guild.Id].token.Cancel();
+				musicPlayers[ctx.Guild.Id].musicQueue.Clear();
+				musicPlayers[ctx.Guild.Id].musicPlaying = false;
+				await musicPlayers[ctx.Guild.Id].GuildConnection.DisconnectAsync();
+				musicPlayers.Remove(ctx.Guild.Id);
+
+				DiscordEmbedBuilder embed = new()
+				{
+					Color = DiscordColor.HotPink,
+					Title = "Stopped",
+				};
+				await ctx.CreateResponseAsync(embed);
+			}
+		}
+
+		public static async Task AddToQueue(LavalinkTrack track, InteractionContext ctx)
+		{
+			musicPlayers[ctx.Guild.Id].musicQueue.Enqueue(track);
+			await Play(track, ctx);
+		}
+
+		public static async Task Skip(InteractionContext ctx)
+		{
+			if (musicPlayers.ContainsKey(ctx.Guild.Id))
+			{
+				musicPlayers[ctx.Guild.Id].token.Cancel();
+				DiscordEmbedBuilder embed = new()
+				{
+					Color = DiscordColor.HotPink,
+					Title = "Song skipped",
+				};
+				await ctx.CreateResponseAsync(embed);
+			}
+		}
+
+		public static async Task SayQueue(InteractionContext ctx)
+		{
+			if (musicPlayers.ContainsKey(ctx.Guild.Id))
+			{
+				DiscordEmbedBuilder embed = new()
+				{
+					Color = DiscordColor.HotPink,
+					Title = "Queue",
+				};
+				if (musicPlayers[ctx.Guild.Id].musicQueue.Count <= 0)
+				{
+					embed.AddField("No tracks in queue", "Add some with `!play`");
+				}
+				foreach (var track in musicPlayers[ctx.Guild.Id].musicQueue)
+				{
+					embed.AddField($"{track.Title} - {track.Author}", track.Length.ToString());
+				}
+				await ctx.CreateResponseAsync(embed);
 			}
 		}
 	}
